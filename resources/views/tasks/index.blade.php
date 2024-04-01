34e3002c33f43ee03f197e062a59f0ccc5b32d28
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2024-04-01 15:32:23 +0700

    read datatbase to view

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -7,5 +7,21 @@
 </head>
 <body>
     <h1>Daftar Tugas-tugas</h1>
+    <table>
+        <tr>
+            <th>#</th>
+            <th>Judul</th>
+            <th>Deskripsi</th>
+            <th>Selesai</th>
+        </tr>
+        @foreach($tasks as $task)
+        <tr>
+            <td>{{ $task->id }}</td>
+            <td>{{ $task->title }}</td>
+            <td>{{ $task->description }}</td>
+            <td>{{ $task->completed }}</td>
+        </tr>
+        @endforeach
+    </table>
 </body>
 </html>
